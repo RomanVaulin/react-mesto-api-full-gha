9e10e51a0d0a4f26e4554fb8094d0b16e5e8dd80
Roman Vaulin <vaulin007@yandex.ru>
2023-11-19 17:26:06 +0500

    pr15_1.2

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const errorHandler = require('./middlewares/handle-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mongodb' } = process.env;
 
 const app = express();
 
